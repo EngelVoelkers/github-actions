2439dc5b454453ae021e121c030d48be69a16589
Wittler, Robin <robin.wittler@engelvoelkers.com>
2021-03-16 15:29:04 +0100

    ULM-6 Do some reformatting and add some output

--- gcp/keep-n-docker-images/keep-n-docker-images.sh
+++ gcp/keep-n-docker-images/keep-n-docker-images.sh
@@ -3,31 +3,31 @@
 IMAGE=$1
 MAX=$2
 
-#echo "DEBUG: This is github home => $( ls -la /github/home )"
-#echo "DEBUG: This is github workplace => $( ls -la /github/workplace )"
-#echo "DEBUG: This is the content of GOOGLE_APPLICATION_CREDENTIALS => ${GOOGLE_APPLICATION_CREDENTIALS}"
-#echo "DEBUG: Show 'ls' for GOOGLE_APPLICATION_CREDENTIALS => $(ls -la $GOOGLE_APPLICATION_CREDENTIALS) "
-#echo "DEBUG: Show gcloud auth list => $(gcloud auth list)"
-#echo "DEBUG: Show gcloud config list => $(gcloud config list)"
 
-
-gcloud auth activate-service-account github-actions@gcp-workplace-dev.iam.gserviceaccount.com --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
+gcloud auth activate-service-account \
+    github-actions@gcp-workplace-dev.iam.gserviceaccount.com \
+    --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
 
 
 read -r -d " " -a output <<< "$(gcloud container images list-tags "$IMAGE" --format="value(TAGS)")"
 echo "Found ${#output[@]} Images of ${IMAGE} in the registry ..."
 if [[ ${#output[@]} -gt ${MAX} ]]; then
-    images_to_delete=$(( ${#output[@]} - "${MAX}" ))
+    images_to_delete="$(( ${#output[@]} - "${MAX}" ))"
     echo "Need to delete ${images_to_delete} Images."
     echo "${output[@]:MAX}" | while read -r -d " " TAG || [ -n "$TAG" ]; do
-        gcloud -q container images delete "${IMAGE}":"${TAG}" --force-delete-tags
-        echo -ne "Deleted Image ${IMAGE}:${TAG}. "
+        gcloud -q container images delete "${IMAGE}":"${TAG}" --force-delete-tags > /dev/null 2>&1
+        if [[ $? != 0 ]]; then
+            echo "There was an error while deleting ${IMAGE}:${TAG}"
+        else
+            echo "Deleted Image ${IMAGE}:${TAG}."
+        fi
     done
 else
     echo "No need to delete images."
-    images_to_delete=0
+    images_to_delete="0"
 fi
 
+echo
 echo "::set-output name=deleted_images::${images_to_delete}"
 
 exit 0
